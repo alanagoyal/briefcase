daef764ddce31eb3d8973b29e5b6fdc7a8337cc6
Jacob Zwang <59858341+JacobZwang@users.noreply.github.com>
2024-07-15 12:59:37 -0700

    nextjs work in deno locally
    Co-authored-by: Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>

--- app/counter/page.tsx
+++ app/counter/page.tsx
@@ -2,6 +2,8 @@ import { useCloud } from "freestyle-sh";
 import { CounterCS } from "@/cloudstate/counter";
 import Counter from "@/components/counter";
 
+export const dynamic = "force-dynamic";
+
 export default async function CounterPage() {
   const counter = useCloud<typeof CounterCS>("counter");
   const count = await counter.getCount();
--- freestyle.config.ts
+++ freestyle.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from "freestyle-sh";
 
 export default defineConfig({
   dev: {
-    command: "npx next dev",
+    command: "deno run -A ./start-deno.ts",
     proxy: "http://localhost:3000",
+  },
+  deploy: {
+    web: {
+      entryPoint: "./start-deno.ts",
+    },
   },
 });
