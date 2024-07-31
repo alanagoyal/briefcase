165ec95bdc986b41bbde5b672ece1cd2561a279b
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2024-07-31 10:15:48 -0700

    improvements

--- freestyleify.sh
+++ freestyleify.sh
@@ -1,7 +1,6 @@
 cp -r public .next/standalone/public
 cp -r .next/static .next/standalone/.next/static
 rm -rf .next/standalone/node_modules
-cp  cache-handler.js .next/standalone/cache-handler.js
 # rm -rf freestylebuild
 # mkdir freestylebuild
 # cp -r .next/standalone/* freestylebuild
