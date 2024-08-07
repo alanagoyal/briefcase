bd8a5fc9c3db860f4b91be4a24991fa2110d8c7e
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2024-08-07 14:59:22 -0700

    small improvements

--- freestyleify.sh
+++ freestyleify.sh
@@ -1,12 +1,6 @@
 cp -r public .next/standalone/public
 cp -r .next/static .next/standalone/.next/static
 rm -rf .next/standalone/node_modules
-# rm -rf freestylebuild
-# mkdir freestylebuild
-# cp -r .next/standalone/* freestylebuild
-# cp -r .next/standalone/.next freestylebuild/
-# # cp deno.json freestylebuild/deno.json
-# rm freestylebuild/package.json
+cp entry.js .next/standalone/entry.js
 
 node monkey-patch.js
-cp entry.js .next/standalone/entry.js
