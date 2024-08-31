dc00ef385483b8629c1ca5ba32042385f7347a0f
alanagoyal <alanaan@gmail.com>
2024-08-31 16:30:52 -0700

    initial setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,12 @@
-import path from "node:path";
-import { createRequire } from "node:module";
+import createNextIntlPlugin from 'next-intl/plugin';
 
-const require = createRequire(import.meta.url);
-import { fileURLToPath } from 'node:url';
+const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
   cleanDistDir: true,
   cacheMaxMemorySize: 0, // disable default in-memory caching
-  
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
