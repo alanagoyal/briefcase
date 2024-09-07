f3c333a5e3f78e0c058c272307a7457ea7e15991
alanagoyal <alanaan@gmail.com>
2024-09-07 16:13:55 -0700

    undo

--- config/site.ts
+++ config/site.ts
@@ -1,11 +1,6 @@
-import { useI18n } from "@quetzallabs/i18n";
-
-const { t } = useI18n();
 export const siteConfig = {
-  name: t("Briefcase"),
-  description: t(
-    "The AI legal assistant for fast-moving founders and investors"
-  ),
+  name: "Briefcase",
+  description: "The AI legal assistant for fast-moving founders and investors",
   url: "https://briefcase-ai.vercel.app",
   ogImage: "https://base-case-images.s3.us-west-1.amazonaws.com/briefcase.png",
 };
