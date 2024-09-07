1ee84099fbb931795df985d481f4da22980f36dd
alanagoyal <alanaan@gmail.com>
2024-09-07 16:08:54 -0700

    change tagline

--- config/site.ts
+++ config/site.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
-    name: "Briefcase",
-    description: "A legal research tool for founders and investors",
-    url: "https://briefcase-ai.vercel.app",
-    ogImage: "https://base-case-images.s3.us-west-1.amazonaws.com/briefcase.png",
-  };
+  name: "Briefcase",
+  description: "The AI legal assistant for fast-moving founders and investors",
+  url: "https://briefcase-ai.vercel.app",
+  ogImage: "https://base-case-images.s3.us-west-1.amazonaws.com/briefcase.png",
+};
