188a8a532099458288de0da3f3efcae5ce465ad0
alanagoyal <alanaan@gmail.com>
2024-09-07 09:13:01 -0700

    light theme default

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default async function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
