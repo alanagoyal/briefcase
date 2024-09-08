a7ac67bb2946b3aeae6e2c45d6827574dfc7a3ed
alanagoyal <alanaan@gmail.com>
2024-09-08 08:20:58 -0700

    resize og

--- config/site.ts
+++ config/site.ts
@@ -2,5 +2,5 @@ export const siteConfig = {
   name: "Briefcase",
   description: "The AI legal assistant for fast-moving founders and investors",
   url: "https://briefcase-ai.vercel.app",
-  ogImage: "https://base-case-images.s3.us-west-1.amazonaws.com/briefcase.png",
+  ogImage: "https://base-case-images.s3.us-west-1.amazonaws.com/briefcase.jpg",
 };
